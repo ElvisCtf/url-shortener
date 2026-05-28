7e39a57931b0ff3af14c1c7cb6d9adcde3918a2a
ElvisCtf <elvischengtf@gmail.com>
2026-05-28 16:43:38 +0800

    feat(redirect-service): Check whether the link is active

--- backend/redirect-service/internal/features/redirect/redirect.repository.go
+++ backend/redirect-service/internal/features/redirect/redirect.repository.go
@@ -1,8 +1,14 @@
 package redirect
 
 import (
+	"errors"
 	"example.com/shared/postgres"
 	"gorm.io/gorm"
+)
+
+var (
+	ErrLinkNotFound = errors.New("link code not found")
+	ErrLinkInactive = errors.New("link is inactive")
 )
 
 type RedirectRepository struct {
@@ -14,20 +20,20 @@ func NewRedirectRepository(db *gorm.DB) *RedirectRepository {
 }
 
 func (r *RedirectRepository) FindByCode(code string) (string, error) {
-	var result struct {
-		OriginalURL string
-	}
+	var link postgres.Link
 
-	err := r.db.
-		Model(&postgres.Link{}).
-		Select("original_url").
-		Where("code = ?", code).
-		Take(&result).Error
+	err := r.db.Where("code = ?", code).First(&link).Error
 
 	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return "", ErrLinkNotFound
+		}
 		return "", err
 	}
 
-	return result.OriginalURL, nil
+	if !link.Active {
+		return "", ErrLinkInactive
+	}
 
+	return link.OriginalURL, nil
 }
